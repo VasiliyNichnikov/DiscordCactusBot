bb324fdc20ad2b4e6f6436a88e1a4912f712e1c4
Vasiliy <v.nichnikov@gmail.com>
2022-07-22 13:09:42 +0300

    Изменена Program.cs

--- DiscordCactusBot/Program.cs
+++ DiscordCactusBot/Program.cs
@@ -3,7 +3,6 @@ using Discord.Commands;
 using Discord.WebSocket;
 using DiscordCactusBot.Commands;
 using DiscordCactusBot.Core;
-using DiscordCactusBot.Database;
 
 namespace DiscordCactusBot;
 
@@ -30,28 +29,6 @@ public class Program
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
         await commandHandler.InstallCommandsAsync();
-
-        // Utils.GetInfoForConnectToDatabase();
-        // await using (var db = new ApplicationContext())
-        // {
-        //     
-        // }
-        // {
-        //     // var channel1 = new ChannelModel {Name = "Channel1"};
-        //     // var channel2 = new ChannelModel {Name = "Channel2"};
-        //     //
-        //     // db.Channels.Add(channel1);
-        //     // db.Channels.Add(channel2);
-        //     //
-        //     // await db.SaveChangesAsync();
-        //     //
-        //     // var channels = db.Channels.ToList();
-        //     // Console.WriteLine($"Список объектов: {channels.Count}");
-        //     // foreach (var channel in channels)
-        //     // {
-        //     //     Console.WriteLine($"Data. Id: {channel.Id}. Name: {channel.Name}");
-        //     // }
-        // }
         
         _client.Ready += () =>
         {
